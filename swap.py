11436bd59386371531131c7ab6423f9ca3270e49
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-23 09:24:30 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,8 +1,14 @@
-a=int(input('Enter a value for A: ');
-b=int(input('Enter a value for B: ');
+a=int(input('Enter a value for A: '));
+b=int(input('Enter a value for B: '));
 a=a+b;
 b=a-b;
 a=a-b;
 print('After swapping a value is : ',a)
 print('After swapping b value is: ',b)
       
+OUTPUT:
+      
+      Enter a value for A: 23
+Enter a value for B: 32
+After swapping a value is :  32
+After swapping b value is:  23
