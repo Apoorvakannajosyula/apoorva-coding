90d9c7777f98d129309bb183683dc2c4358ed4bf
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-23 09:22:35 +0530

    Update student.py

--- student.py
+++ student.py
@@ -4,3 +4,25 @@ gender=input('Enter student gender: ')
 group=input('Enter student group:')
 address=input('Enter student address:')
 phno=int(input('Enter student phone number: '))        
+print('Student no:',sno)
+print('Student name:',name)
+print('Student gender:',gender)
+print('Student group:',group)
+print('Student address: ',address)
+print('student phone no:',phno)
+
+output:
+  
+  Enter student no: 02
+Enter student name: apoorva
+Enter student gender: female
+Enter student group:bca
+Enter student address:vizianagram
+Enter student phone number:9876543210
+Student no: 2
+Student name: apoorva
+Student gender: female
+Student group: bca
+Student address:  vizianagram
+student phone no: 876543210
+> 
