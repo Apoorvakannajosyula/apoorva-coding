1bdf3872958823c60e2772e5aa11342948d8a110
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-24 07:02:51 +0530

    Create posi.py

---
+++ posi.py
@@ -0,0 +1,16 @@
+a=int(input('Enter a value for A :'))
+if a>=0:
+    print('A is +ve')
+else:
+    print('A is -ve')
+    
+    OUTPUT:
+      
+      
+      Enter a value for A :23
+A is +ve
+> 
+
+Enter a value for A :-9
+A is -ve
+> 
