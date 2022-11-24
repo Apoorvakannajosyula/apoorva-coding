ba56901a88298fa4dfdf685af3f06c3fc666721c
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-24 06:54:08 +0530

    Create equal.py

---
+++ equal.py
@@ -0,0 +1,20 @@
+a=int(input('Enter a value for A :'))
+b=int(input('Enter a value for B :'))
+if a==b:
+    print('A and B are equal')
+else:
+    print('A and B are not equal')
+    
+    
+    OUTPUT:
+      
+      
+      Enter a value for A :23
+Enter a value for B :23
+A and B are equal
+> 
+
+Enter a value for A :23
+3Enter a value for B :2
+A and B are not equal
+> 
