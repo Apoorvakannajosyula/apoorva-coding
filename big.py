39fea09ee8d449bb22ea4a258eb27012830ac0f3
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-24 06:51:49 +0530

    Create big.py

---
+++ big.py
@@ -0,0 +1,14 @@
+a=int(input('Enter a value for A :'))
+b=int(input('Enter a value for B :'))
+if a>b:
+    print('A is big')
+else:
+    print('B is big')
+    
+    
+    OUTPUT:
+      
+      Enter a value for A :23
+Enter a value for B :32
+B is big
+> 
