932a88b73bce3192a007fea2fe2c7ef43dff344e
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-24 07:00:49 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,12 @@
+a=int(input('Enter a value for A :'))
+if a%2==0:
+    print('A is even')
+else:
+    print('A is odd')
+    
+    
+    OUTPUT:
+      
+      Enter a value for A :24
+A is even
+> 
