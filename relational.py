792ff95d3c604f155367bd47d1b33f3d36edc0bc
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-25 06:12:22 +0530

    Create relational.py

---
+++ relational.py
@@ -0,0 +1,21 @@
+a=int(input('Enter a number: '))
+b=int(input('Enter a number: '))
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+
+OUTPUT:
+  
+  Enter a number: 5
+Enter a number: 2
+True
+False
+True
+False
+False
+True
+> 
