5e87dfb870d0b8f918e7ab885a0202b8d6ab2893
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-25 06:09:18 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,24 @@
+a=int(input('Enter a number: '))
+b=int(input('Enter a number: '))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+
+OUTPUT:
+  
+  
+  Enter a number: 2
+Enter a number: 3
+5
+-1
+6
+0.6666666666666666
+2
+8
+0
+> 
