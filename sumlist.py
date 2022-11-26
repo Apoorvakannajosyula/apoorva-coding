30350eba6478fb24eafe4938448b7bd56cb39ab5
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-26 10:11:34 +0530

    Create sumlist.py

---
+++ sumlist.py
@@ -0,0 +1,11 @@
+list1=[1,2,3,4,5]
+count=0
+for i in list1:
+    if i%2==0:
+        count=count+i
+print('the sum of even number elements in the list is : ',count)        
+
+
+OUTPUT:
+  
+  the sum of even number elements in the list is :  6
