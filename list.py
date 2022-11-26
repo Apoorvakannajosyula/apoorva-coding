7d41e4574aaa7b87447f3ee8f2a825fb9800172d
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-26 10:03:20 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,19 @@
+list1=[1,2,3,4,5]
+print("enter a number")
+n=int(input())
+if n in list1:
+ print("Yes")
+else:
+ print ("no")    
+
+
+OUTPUT:
+  
+  enter a number
+5
+Yes
+
+enter a number
+7
+no
+> 
