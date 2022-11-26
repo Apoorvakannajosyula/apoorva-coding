bffd373957f7c466c8300c0152c80921b912f37e
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-26 20:36:04 +0530

    Create bit.py

---
+++ bit.py
@@ -0,0 +1,14 @@
+a=int(input('Enter a value for A: '))
+b=int(input('Enter a value for B: '))
+print('The bitwise and operation of A and B is :',a&b)
+print('The bitwise or operation of A and B is :',a|b)
+print('The bitwise xor operation of A and B is :',a^b)
+
+
+OUTPUT:
+  
+  Enter a value for A: 10
+Enter a value for B: 12
+The bitwise and operation of A and B is : 8
+The bitwise or operation of A and B is : 14
+The bitwise xor operation of A and B is : 6
