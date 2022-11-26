2db518391fa1b3c15415be17cdf5bb242ee223e6
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-26 20:42:00 +0530

    Create leftshift.py

---
+++ leftshift.py
@@ -0,0 +1,30 @@
+n=int(input("Enter a number:"))
+print('The left shift operator from 1 to 10 of the given number is :')
+print(n<<1)
+print(n<<2)
+print(n<<3)
+print(n<<4)
+print(n<<5)
+print(n<<6)
+print(n<<7)
+print(n<<8)
+print(n<<9)
+print(n<<10)
+
+
+OUTPUT:
+  
+  
+  Enter a number:10
+The left shift operator from 1 to 10 of the given number is :
+20
+40
+80
+160
+320
+640
+1280
+2560
+5120
+10240
+> 
