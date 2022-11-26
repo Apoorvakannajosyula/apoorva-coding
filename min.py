20d22abbad9b0eb1399014f2f764ac0f8bcce809
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-26 10:08:42 +0530

    Create min.py

---
+++ min.py
@@ -0,0 +1,12 @@
+list1=[1,2,3,4,5]
+min=list1[0]
+for i in list1:
+    if i<min:
+        min=i
+print('the minimum element in the list is : ',min)        
+
+
+OUTPUT:
+  
+  
+  the minimum element in the list is :  1
