9ec00fe4e5f5353c448d0aa7a300a2a3735da3ce
Apoorvakannajosyula <111000787+Apoorvakannajosyula@users.noreply.github.com>
2022-11-26 20:43:58 +0530

    Create rightshift.py

---
+++ rightshift.py
@@ -0,0 +1,28 @@
+n=int(input("Enter a number:"))
+print('The right shift operator from 1 to 10 of the given number is :')
+print(n>>1)
+print(n>>2)
+print(n>>3)
+print(n>>4)
+print(n>>5)
+print(n>>6)
+print(n>>7)
+print(n>>8)
+print(n>>9)
+print(n>>10)
+
+
+OUTPUT:
+  
+  Enter a number:100
+The right shift operator from 1 to 10 of the given number is :
+50
+25
+12
+6
+3
+1
+0
+0
+0
+0
